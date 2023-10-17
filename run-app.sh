4d0730810bdb11d876546da157245ff7eade7529
Enrico Daga <enrico.daga@open.ac.uk>
2023-10-17 11:23:12 +0200

    changes to scripts

--- run-app.sh
+++ run-app.sh
@@ -2,4 +2,6 @@
 # python3 -m venv .python3
 source .python3/bin/activate
 export FLASK_APP=app.py
-flask --debug run
+#flask --debug run
+nohup python3 app.py > run-app.log 2>&1 &
+echo $!> run-app.pid
